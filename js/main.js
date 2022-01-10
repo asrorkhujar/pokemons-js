e2bcc02ee6c81ea2f09cb9bebe4dcd12f7719bf9
coder-1990 <asrorakmalovich@gmail.com>
2022-01-11 01:30:50 +0500

    add button like

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ for (let pokemon of pokemons) {
   let newCardTopInfoWrapper = document.createElement('div');
   let newCardTitle = document.createElement('h5');
   let newCardType = document.createElement('p');
+  let newCardLikeBtn = document.createElement('button');
   let newHeartImg = document.createElement('img');
   let newCardBottomInfoWrapper = document.createElement('div');
   let newCardWeight = document.createElement('p');
@@ -24,17 +25,17 @@ for (let pokemon of pokemons) {
   let newCardWeaknesses = document.createElement('p');
 
   //SET ATTRIBUTE
-  newItemPokemon.setAttribute('class', 'pokemons__item col-sm-6 col-md-3 mb-4');
+  newItemPokemon.setAttribute('class', 'pokemons__item col-sm-6 col-md-3 mb-5');
   newCard.setAttribute('class', 'card pokemons__card pt-3 border-2 border-dark h-100');
   newCard.setAttribute('style', 'border-radius: 20px; width: 18rem;');
   newImgWrapper.setAttribute('class', 'pb-5 mx-auto');
   newImg.setAttribute('class', 'card-img-top img-fluid');
   newImg.setAttribute('src', pokemon.img);
   newCardBody.setAttribute('class', 'card-body px-4 border-top border-2 border-dark d-flex flex-column');
-  newCardContentWrapper.setAttribute('class', 'd-flex justify-content-between');
-
+  newCardContentWrapper.setAttribute('class', 'd-flex justify-content-between align-items-start');
   newCardTitle.setAttribute('class', 'card-title h4 fw-bold mb-0');
-
+  newCardLikeBtn.setAttribute('class', 'bg-transparent border-0');
+  newCardLikeBtn.setAttribute('type', 'button');
   newHeartImg.setAttribute('src', '/img/heart.svg');
   newHeartImg.setAttribute('width', '30');
   newHeartImg.setAttribute('height', '30');
@@ -47,7 +48,7 @@ for (let pokemon of pokemons) {
   newCardTitle.textContent = pokemon.name;
   newCardType.textContent = pokemon.type.join(', ');
   newCardWeight.textContent = pokemon.weight;
-  newCardAge.textContent = `${pokemon.candy_count} age`;
+  newCardAge.textContent = pokemon.candy_count ? `${pokemon.candy_count} age` : 'Age is unknown';
   newCardWeaknesses.textContent = pokemon.weaknesses.join(', ');
 
   //APPEND CHILD
@@ -60,7 +61,8 @@ for (let pokemon of pokemons) {
   newCardContentWrapper.appendChild(newCardTopInfoWrapper);
   newCardTopInfoWrapper.appendChild(newCardTitle);
   newCardTopInfoWrapper.appendChild(newCardType);
-  newCardContentWrapper.appendChild(newHeartImg);
+  newCardContentWrapper.appendChild(newCardLikeBtn);
+  newCardLikeBtn.appendChild(newHeartImg);
   newCardBody.appendChild(newCardBottomInfoWrapper);
   newCardBottomInfoWrapper.appendChild(newCardWeight);
   newCardBottomInfoWrapper.appendChild(newCardAge);
